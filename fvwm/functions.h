29a71f804068d58de0139d8c754a02379c0bcac5
domivogt <domivogt>
1999-03-07 14:31:05 +0000

    ADDED FUNCTIONS.H

---
+++ fvwm/functions.h
@@ -0,0 +1,36 @@
+#ifndef _FUNCTION_
+#define _FUNCTION_
+
+struct FvwmFunction;               /* forward declaration */
+
+typedef struct FunctionItem
+{
+  struct FvwmFunction *func;       /* the menu this item is in */
+  struct FunctionItem *next_item;  /* next function item */
+  char condition;                  /* the character string displayed on left*/
+  char *action;                    /* action to be performed */
+  short type;                      /* type of built in function */
+  Bool f_needs_window;
+} FunctionItem;
+
+typedef struct FvwmFunction
+{
+  struct FvwmFunction *next_func;  /* next in list of root menus */
+  FunctionItem *first_item;        /* first item in function */
+  FunctionItem *last_item;         /* last item in function */
+  char *name;                      /* function name */
+  unsigned int use_depth;
+} FvwmFunction;
+
+
+#define FUNC_NO_WINDOW False
+#define FUNC_NEEDS_WINDOW True
+
+/* Types of events for the FUNCTION builtin */
+#define MOTION                'm'
+#define IMMEDIATE             'i'
+#define CLICK                 'c'
+#define DOUBLE_CLICK          'd'
+#define ONE_AND_A_HALF_CLICKS 'o'
+
+#endif /* _FUNCTION_ */
