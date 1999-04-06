33446028c6939763f23dec8cc9f9ba264e3e93e7
clasen <clasen>
1999-04-06 19:11:21 +0000

    Remove EXCEED_HACK #define, second try.

--- acconfig.h
+++ acconfig.h
@@ -113,10 +113,6 @@
 /* Enables session management functionality. */
 #undef SESSION
 
-/* Enables a hack to raise X windows over native windows when
-   using the Exceed X Server under NT */
-#undef EXCEED_HACK
-
 /* Specify a type for sig_atomic_t if it's not available.  */
 #undef sig_atomic_t
 
