bc34a5e0fb96c2458bd729e81597745f87b07773
migo <migo>
1999-06-26 23:48:29 +0000

    * Form for setting root cursor.

---
+++ modules/FvwmForm/FormFvwmRootCursor.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+$CHOICES_IN_LINE = 4;
+$CURSORFONT_H = "/usr/X11/include/X11/cursorfont.h";
+
+die "Can't find cursorfont.h, tried '$CURSORFONT_H'.\n"
+  unless -r $CURSORFONT_H;
+
+open(CFH, "<$CURSORFONT_H");
+$cfContent = join('', <CFH>);
+close(CFH);
+
+@cursorFonts = $cfContent =~ /XC_([^\s]*)\s/sg;
+@cursorFonts = grep(!/num_glyphs/, @cursorFonts);
+
+$len = 0; map { $len = length if $len < length } @cursorFonts;
+$include = "";
+$count = $CHOICES_IN_LINE;
+
+foreach (@cursorFonts) {
+  if ($count == $CHOICES_IN_LINE) {
+    $include .= "*FormFvwmRootCursor.Line   expand\n"; $count = 0;
+  }
+  $include .= sprintf("*FormFvwmRootCursor.Choice cursor_font %-${len}s off \"%-${len}s\"\n", $_, $_);
+  $count++;
+}
+
+$_ = join('', <DATA>);
+s/\@INCLUDE_STANDARD_CURSOR_FONTS@\n/$include/s;
+print $_;
+
+# ============================================================================
+# Here is the actual FormFvwmRootCursor. template goes
+__DATA__
+# Front end to xsetroot, sets root cursor font and color.
+
+DestroyModuleConfig FormFvwmRootCursor.*
+*FormFvwmRootCursor.WarpPointer
+#*FormFvwmRootCursor.Font	fixed
+*FormFvwmRootCursor.ButtonFont	6x13
+#*FormFvwmRootCursor.Fore	yellow
+#*FormFvwmRootCursor.Back	navy
+#*FormFvwmRootCursor.ItemFore	black
+#*FormFvwmRootCursor.ItemBack	#20D020
+
+*FormFvwmRootCursor.Text   "Cursor font"
+*FormFvwmRootCursor.Selection	cursor_font single
+
+@INCLUDE_STANDARD_CURSOR_FONTS@
+
+*FormFvwmRootCursor.Line	expand
+*FormFvwmRootCursor.Text   "Cursor inner color"
+*FormFvwmRootCursor.Line	expand
+
+*FormFvwmRootCursor.Selection      cursor_fg single
+*FormFvwmRootCursor.Line   center
+*FormFvwmRootCursor.Choice cursor_fg black   on  "  black   "
+*FormFvwmRootCursor.Choice cursor_fg red     off "  red     "
+*FormFvwmRootCursor.Choice cursor_fg green   off "  green   "
+*FormFvwmRootCursor.Choice cursor_fg blue    off "  blue    "
+*FormFvwmRootCursor.Line   center
+*FormFvwmRootCursor.Choice cursor_fg navy    off "  navy    "
+*FormFvwmRootCursor.Choice cursor_fg cyan    off "  cyan    "
+*FormFvwmRootCursor.Choice cursor_fg yellow  off "  yellow  "
+*FormFvwmRootCursor.Choice cursor_fg white   off "  white   "
+
+*FormFvwmRootCursor.Line	expand
+*FormFvwmRootCursor.Text   "Cursor outer color"
+*FormFvwmRootCursor.Line	expand
+
+*FormFvwmRootCursor.Selection      cursor_bg single
+*FormFvwmRootCursor.Line   center
+*FormFvwmRootCursor.Choice cursor_bg black   off "  black   "
+*FormFvwmRootCursor.Choice cursor_bg red     off "  red     "
+*FormFvwmRootCursor.Choice cursor_bg green   off "  green   "
+*FormFvwmRootCursor.Choice cursor_bg blue    off "  blue    "
+*FormFvwmRootCursor.Line   center
+*FormFvwmRootCursor.Choice cursor_bg navy    off "  navy    "
+*FormFvwmRootCursor.Choice cursor_bg cyan    off "  cyan    "
+*FormFvwmRootCursor.Choice cursor_bg yellow  off "  yellow  "
+*FormFvwmRootCursor.Choice cursor_bg white   on  "  white   "
+
+*FormFvwmRootCursor.Line	expand
+*FormFvwmRootCursor.Line	expand
+
+*FormFvwmRootCursor.Button	continue "Set Root Cursor"
+*FormFvwmRootCursor.Command	Exec exec xsetroot -cursor_name $(cursor_font!none) -fg $(cursor_fg) -bg $(cursor_bg)
+*FormFvwmRootCursor.Button	quit "Finish" ^[
+*FormFvwmRootCursor.Command	Nop
