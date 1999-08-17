13b108ab9e485aed13fe8ce33d97c87979f188c6
migo <migo>
1999-08-17 21:27:50 +0000

    * BuildXLockMenu: the compatibility wrapper to fvwm-menu-xlock * restored old menu shell scripts used as examples in the man page * some small documentation fixes

---
+++ utils/fvwm_make_browse_menu.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+#
+###
+#  This script demonstates the fvwm menu DynamicPopupAction functionality.
+#  You can use a more configurable fvwm-menu-directory instead.
+#  The line below almost exactly simulates this script:
+#    fvwm-menu-directory --reuse --links --order 4 --exec-file vi --name NAME
+###
+#
+#  Modification History
+#
+#  Created on 05/06/99 by Dominik Vogt (domivogt):
+#
+#  provides output to read in with PipeRead to build a menu
+#  containing a directory listing.
+#
+#  You should put these lines into your fvwm configuration file to invoke
+#  this script:
+#
+#  AddToMenu <menu name>
+#  + DynamicPopupAction Piperead 'fvwm_make_browse_menu.sh <menu name>
+
+#
+# configuration section
+#
+
+# the file containing the desired directory name
+DIRFILE="$HOME/.fvwm_browse_menu_cwd"
+
+# the name of our menu
+MENU="$1"
+
+# the command to execute on plain files
+ACTION=vi
+
+# the terminal program to invoke
+TERMINAL="xterm -e"
+
+# you may use the absolute path here if you have an alias like ls="ls -F"
+LS=ls
+
+SED=sed
+
+#
+# you may customize this script below here.
+#
+
+# create a file containing the current working directory
+if [ ! -f "$DIRFILE" ] ; then
+    echo "$HOME" > "$DIRFILE"
+fi
+
+# get desired directory
+DIR="`cat "$DIRFILE"`"
+if [ ! -d "$DIR" ] ; then
+    DIR="$HOME"
+fi
+
+# dump all menu items
+echo DestroyMenu recreate \"$MENU\"
+
+# add a new title
+echo AddToMenu \"$MENU\" \"`cat "$DIRFILE"`\" Title
+
+# add '..' entry
+cd "$DIR"/..
+echo AddToMenu \"$MENU\" \"..\" PipeRead \'echo \""$PWD"\" \> "$DIRFILE" ';' echo Menu \\\""$MENU"\\\" WarpTitle\'
+
+# add separator
+echo AddToMenu \"$MENU\" \"\" Nop
+
+# add $HOME entry
+echo AddToMenu \"$MENU\" \"~\" PipeRead \'echo \""$HOME"\" \> "$DIRFILE" ';' echo Menu \\\""$MENU"\\\" WarpTitle\'
+
+# add separator
+echo AddToMenu \"$MENU\" \"\" Nop
+
+# add directory contents
+for i in `"$LS" "$DIR"` ; do
+    if [ -d "$DIR/$i" ] ; then
+        # it's a directory
+        cd "$DIR/$i"
+        # put new path in $DIRFILE and invoke the menu again
+        echo AddToMenu \"$MENU\" \""$i/"\" PipeRead \'echo \"`echo $PWD|$SED -e s://:/:g`\" \> "$DIRFILE" ';' echo Menu \\\""$MENU"\\\" WarpTitle\'
+    else
+        # something else, apply $ACTION to it
+        echo AddToMenu \"$MENU\" \""$i"\" Exec $TERMINAL $ACTION \""$DIR/$i"\"
+    fi
+done
---
+++ utils/fvwm_make_directory_menu.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+#
+###
+#  This script demonstates the fvwm menu MissingSubmenuFunction functionality.
+#  You can use a more configurable fvwm-menu-directory instead.
+#  The line below almost exactly simulates this script:
+#    fvwm-menu-directory --links --order 4 --exec-file vi --dir DIR
+###
+#
+#  Modification History
+#
+#  Created on 06/07/99 by Dominik Vogt (domivogt):
+#
+#  provides output to read in with PipeRead to build a menu
+#  containing a directory listing.
+#
+#  You should put these lines into your fvwm configuration file to invoke
+#  this script:
+#
+#  AddToFunc MakeMissingDirectoryMenu
+#  + i piperead 'fvwm_make_directory_menu.sh '$0
+#
+#  And put these lines in the menu from which you want to pop up the
+#  directory menus:
+#
+#  AddToMenu SomeMenu MissingSubmenuFunction MakeMissingDirectoryMenu
+#  + "my directory item" Popup /whatever/path/you/like
+#
+#  Note: please use absolute path names.
+
+#
+# configuration section
+#
+
+# If you want to optimize for speed, unset this variable. Warning: speed
+# optimization takes up a lot of memory that is never free'd again while fvwm
+# is running.
+OPTIMIZE_SPACE=yes
+
+# you may use the absolute path here if you have an alias like ls="ls -F"
+LS="/bin/ls -LF"
+SED=sed
+
+# the name of our menu
+MENU=`echo $1 | $SED -e 's://*:/:g'`
+
+# the item title
+ITEM='basename $MENU'
+
+# the command to execute on plain files
+ACTION=vi
+
+# the terminal program to invoke
+TERMINAL="xterm -e"
+TERMINALSHELL="xterm"
+
+#
+# you may customize this script below here.
+#
+
+# dump all menu items
+echo DestroyMenu recreate \"$MENU\"
+
+# add a new title
+echo AddToMenu \"$MENU\" \"$MENU\" Exec "echo cd $MENU\; $TERMINALSHELL | $SHELL"
+
+# add a separator
+echo AddToMenu \"$MENU\" \"\" nop
+
+# destroy the menu after it is popped down
+if [ "$OPTIMIZE_SPACE" = yes ] ; then
+  echo AddToMenu \"$MENU\" DynamicPopDownAction DestroyMenu \"$MENU\"
+fi
+
+# set the 'missing submenu function'
+echo AddToMenu \"$MENU\" MissingSubmenuFunction MakeMissingDirectoryMenu
+
+# add directory contents
+$LS "$MENU" 2> /dev/null | $SED -n '
+  /\/$/ bdirectory
+  s:[=*@|]$::1
+  s:^\(.*\)$:AddToMenu "'$MENU'" "\1" Exec '"$TERMINAL $ACTION"' "'"$MENU"'/\1":1
+  bnext
+  :directory
+  s:^\(.*\)/$:AddToMenu "'$MENU'" "\1" Popup "'"$MENU"'/\1" item +100 c:1
+  :next
+  s://*:/:g
+  p
+'
