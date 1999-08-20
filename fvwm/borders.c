89353a86aeb48370ed0a99a4afe8dd4aba9e540f
domivogt <domivogt>
1999-08-20 00:15:19 +0000

    * Fixed title drawing.

--- fvwm/borders.c
+++ fvwm/borders.c
@@ -1052,7 +1052,7 @@ void SetTitleBar (FvwmWindow *t,Bool onoroff, Bool NewTitle)
 
   /* for mono, we clear an area in the title bar where the window
    * title goes, so that its more legible. For color, no need */
-  if(Pdepth<20)
+  if(Pdepth<2)
   {
     RelieveRectangle(dpy,t->title_w,0,0,hor_off-3,t->title_g.height - 1,
                      ReliefGC, ShadowGC, rwidth);
