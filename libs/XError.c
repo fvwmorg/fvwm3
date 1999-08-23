720dd6aa666cac6f87a638c91639a4c7bfff0911
hippo <hippo>
1999-08-23 11:57:24 +0000

    PrintXErrorAndCoredump() prints the name of the request code

--- libs/XError.c
+++ libs/XError.c
@@ -1,8 +1,8 @@
-/* Copyright (C) 1999  Dominik Vogt
+/* Copyright (C)",
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,7 +12,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc.,",
  */
 
 /*
@@ -23,16 +23,18 @@
 #include <stdio.h>
 #include <X11/Xlib.h>
 
+static char *request_name(unsigned char code);
+
 void PrintXErrorAndCoredump(Display *dpy, XErrorEvent *error, char *MyName)
 {
   char msg[256];
 
-  XGetErrorText(dpy, error->error_code, msg, 256);
+  XGetErrorText(dpy, error->error_code, msg, sizeof(msg));
 
   fprintf(stderr,"%s: Cause of next X Error.\n", MyName);
   fprintf(stderr, "   Error: %d (%s)\n", error->error_code, msg);
-  fprintf(stderr, "   Major opcode of failed request:  %d \n",
-	  error->request_code);
+  fprintf(stderr, "   Major opcode of failed request:  %d (%s)\n",
+	  error->request_code, request_name(error->request_code));
   fprintf(stderr, "   Minor opcode of failed request:  %d \n",
 	  error->minor_code);
   fprintf(stderr, "   Resource id of failed request:  0x%lx \n",
@@ -43,4 +45,135 @@ void PrintXErrorAndCoredump(Display *dpy, XErrorEvent *error, char *MyName)
   abort();
   /* exit if this fails */
   exit(99);
+}
+
+/* this comes out of Xproto.h */
+static char *code_names[] = {
+  "CreateWindow",
+  "ChangeWindowAttributes",
+  "GetWindowAttributes",
+  "DestroyWindow",
+  "DestroySubwindows",
+  "ChangeSaveSet",
+  "ReparentWindow",
+  "MapWindow",
+  "MapSubwindows",
+  "UnmapWindow",
+  "UnmapSubwindows",
+  "ConfigureWindow",
+  "CirculateWindow",
+  "GetGeometry",
+  "QueryTree",
+  "InternAtom",
+  "GetAtomName",
+  "ChangeProperty",
+  "DeleteProperty",
+  "GetProperty",
+  "ListProperties",
+  "SetSelectionOwner",
+  "GetSelectionOwner",
+  "ConvertSelection",
+  "SendEvent",
+  "GrabPointer",
+  "UngrabPointer",
+  "GrabButton",
+  "UngrabButton",
+  "ChangeActivePointerGrab",
+  "GrabKeyboard",
+  "UngrabKeyboard",
+  "GrabKey",
+  "UngrabKey",
+  "AllowEvents",
+  "GrabServer",
+  "UngrabServer",
+  "QueryPointer",
+  "GetMotionEvents",
+  "TranslateCoords",
+  "WarpPointer",
+  "SetInputFocus",
+  "GetInputFocus",
+  "QueryKeymap",
+  "OpenFont",
+  "CloseFont",
+  "QueryFont",
+  "QueryTextExtents",
+  "ListFonts",
+  "ListFontsWithInfo",
+  "SetFontPath",
+  "GetFontPath",
+  "CreatePixmap",
+  "FreePixmap",
+  "CreateGC",
+  "ChangeGC",
+  "CopyGC",
+  "SetDashes",
+  "SetClipRectangles",
+  "FreeGC",
+  "ClearArea",
+  "CopyArea",
+  "CopyPlane",
+  "PolyPoint",
+  "PolyLine",
+  "PolySegment",
+  "PolyRectangle",
+  "PolyArc",
+  "FillPoly",
+  "PolyFillRectangle",
+  "PolyFillArc",
+  "PutImage",
+  "GetImage",
+  "PolyText",
+  "PolyText1",
+  "ImageText",
+  "ImageText1",
+  "CreateColormap",
+  "FreeColormap",
+  "CopyColormapAndFree",
+  "InstallColormap",
+  "UninstallColormap",
+  "ListInstalledColormaps",
+  "AllocColor",
+  "AllocNamedColor",
+  "AllocColorCells",
+  "AllocColorPlanes",
+  "FreeColors",
+  "StoreColors",
+  "StoreNamedColor",
+  "QueryColors",
+  "LookupColor",
+  "CreateCursor",
+  "CreateGlyphCursor",
+  "FreeCursor",
+  "RecolorCursor",
+  "QueryBestSize",
+  "QueryExtension",
+  "ListExtensions",
+  "ChangeKeyboardMapping",
+  "GetKeyboardMapping",
+  "ChangeKeyboardControl",
+  "GetKeyboardControl",
+  "Bell",
+  "ChangePointerControl",
+  "GetPointerControl",
+  "SetScreenSaver",
+  "GetScreenSaver",
+  "ChangeHosts",
+  "ListHosts",
+  "SetAccessControl",
+  "SetCloseDownMode",
+  "KillClient",
+  "RotateProperties",
+  "ForceScreenSaver",
+  "SetPointerMapping",
+  "GetPointerMapping",
+  "SetModifierMapping",
+  "GetModifierMapping"
+};
+
+   
+static char *request_name(unsigned char code)
+{
+  if (code > (sizeof(code_names) / sizeof(char *)))
+    return "Unknown";
+  return code_names[code - 1];
 }
