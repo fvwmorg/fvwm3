180fa10de672cb57a99b4daf73471f4dd366afde
hippo <hippo>
2000-02-01 16:13:04 +0000

    Fixed FvwmCommand pipes being removed

--- modules/FvwmCommand/FvwmCommandS.c
+++ modules/FvwmCommand/FvwmCommandS.c
@@ -32,38 +32,40 @@
 #define MYNAME   "FvwmCommandS"
 #define MAXHOSTNAME 255
 
-int  Fd[2];  /* pipe to fvwm */
-int  Ffdr;   /* command fifo file discriptors */
-int  Ffdw;   /* message fifo file discriptors */
-char *F_name, *Fc_name, *Fm_name; /* fifo name */
+int Fd[2]; /* pipes to fvwm */
+int FfdC; /* command fifo file discriptors */
+int FfdM; /* message fifo file discriptors */
+
+char *FfdC_name, *FfdM_name; /* fifo names */
+
+ino_t FfdC_ino, FfdM_ino; /* fifo inode numbers */
 
 char client[MAXHOSTNAME];
 char hostname[32];
 
-int  open_fifos(const char *f_stem);
+int open_fifos(const char *f_stem);
 void close_fifos(void);
 void close_pipes(void);
-void DeadPipe( int ) __attribute__((noreturn));
 void err_msg(const char *msg);
 void err_quit(const char *msg) __attribute__((noreturn));
 void process_message(unsigned long type,unsigned long *body);
-void relay_packet( unsigned long, unsigned long, unsigned long *);
-void server( char * );
-static RETSIGTYPE sig_handler( int );
-int  write_f(int fd, char *p, int len);
+void relay_packet(unsigned long, unsigned long, unsigned long *);
+void server(char *);
+static RETSIGTYPE sig_handler(int);
+int write_f(int fd, char *p, int len);
 
 int main(int argc, char *argv[])
 {
   char *fifoname;
 
-  if(argc < FARGS)
+  if (argc < FARGS)
   {
     fprintf(stderr,"%s Version %s should only be executed by fvwm!\n",
 	    MYNAME, MYVERSION);
     exit(1);
   }
 
-  if( argc == FARGS+1 )
+  if (argc == FARGS + 1)
   {
     fifoname = argv[FARGS];
   }
@@ -97,9 +99,8 @@ int main(int argc, char *argv[])
   }
 #else
 #ifdef USE_BSD_SIGNALS
-  fvwmSetSignalMask( sigmask(SIGINT) | sigmask(SIGHUP) |
-                     sigmask(SIGQUIT) | sigmask(SIGTERM) |
-                     sigmask(SIGPIPE) );
+  fvwmSetSignalMask(sigmask(SIGINT) | sigmask(SIGHUP) | sigmask(SIGQUIT)
+                    | sigmask(SIGTERM) | sigmask(SIGPIPE));
 #endif
   signal(SIGPIPE, sig_handler);
   signal(SIGINT, sig_handler);
@@ -121,19 +122,8 @@ int main(int argc, char *argv[])
   /* tell fvwm we're running */
   SendFinishedStartupNotification(Fd);
 
-  server( fifoname );
+  server(fifoname);
   return 1;
-}
-
-/*
- * NOT a signal handler (can't call fprintf in a signal handler)
- */
-void DeadPipe( int dummy )
-{
-  (void)dummy;
-
-  close_pipes();
-  exit(0);
 }
 
 /*
@@ -149,7 +139,7 @@ sig_handler(int signo)
 /*
  * setup server and communicate with fvwm and the client
  */
-void server ( char *name )
+void server (char *name)
 {
   char *home;
   char *f_stem;
@@ -159,32 +149,33 @@ void server ( char *name )
   char cmd[MAX_MODULE_INPUT_TEXT_LEN + 1];
   int  ix,cix;
 
-  if( name == NULL )
+  if (name == NULL)
   {
     char *dpy_name;
 
     /* default name */
     home = getenv("HOME");
-    if (!home)  home = "";
-    f_stem = safemalloc( strlen(home) + strlen(F_NAME) + MAXHOSTNAME + 4);
-    strcpy (f_stem, home);
+    if  (home == NULL)
+      home = "";
+    f_stem = safemalloc(strlen(home) + strlen(F_NAME) + MAXHOSTNAME + 4);
+    strcpy(f_stem, home);
     if (f_stem[strlen(f_stem)-1] != '/')
     {
-      strcat (f_stem, "/");
+      strcat(f_stem, "/");
     }
-    strcat (f_stem, F_NAME);
+    strcat(f_stem, F_NAME);
 
     /* Make it unique */
-    strcat (f_stem, "-");
+    strcat(f_stem, "-");
     gethostname(hostname,32);
     dpy_name = getenv("DISPLAY");
     if (!dpy_name)
       dpy_name = ":0";
     if (strncmp(dpy_name, "unix:", 5) == 0)
       dpy_name += 4;
-    if (!dpy_name[0]  ||  ':' == dpy_name[0])
-      strcat( f_stem, hostname );  /* Put hostname before dpy if not there */
-    strcat (f_stem, client);
+    if (!dpy_name[0] || ':' == dpy_name[0])
+      strcat(f_stem, hostname );  /* Put hostname before dpy if not there */
+    strcat(f_stem, client);
   }
   else
   {
@@ -195,63 +186,57 @@ void server ( char *name )
   {
     exit (-1);
   }
-  SendText(Fd," ",0); /* tell fvwm that we are here */
+  SendText(Fd, "NOP", 0); /* tell fvwm that we are here */
 
   cix = 0;
 
-  while ( !isTerminated )
+  while (!isTerminated)
   {
     FD_ZERO(&fdset);
-    FD_SET(Ffdr, &fdset);
+    FD_SET(FfdC, &fdset);
     FD_SET(Fd[1], &fdset);
 
     if (fvwmSelect(FD_SETSIZE, &fdset, 0, 0, NULL) < 0)
-    {
       if (errno == EINTR)
-      {
         continue;
-      }
-    }
 
     if (FD_ISSET(Fd[1], &fdset))
     {
       FvwmPacket* packet = ReadFvwmPacket(Fd[1]);
-      if ( packet == NULL )
+
+      if (packet == NULL)
       {
         close_pipes();
-        exit( 0 );
+        exit(0);
       }
-      process_message( packet->type, packet->body );
+      process_message(packet->type, packet->body);
     }
 
-    if (FD_ISSET(Ffdr, &fdset))
+    if (FD_ISSET(FfdC, &fdset))
     {
-      len = read( Ffdr, buf, MAX_MODULE_INPUT_TEXT_LEN );
+      len = read(FfdC, buf, MAX_MODULE_INPUT_TEXT_LEN);
       if (len == 0)
-      {
         continue;
-      }
-      if (len < 0)
+      else if (len < 0)
       {
         if (errno != EAGAIN && errno != EINTR)
-	{
           err_quit("reading fifo");
-        }
+        continue;
       }
 
       /* in case of multiple long lines */
-      for (ix=0; ix<len; ix++)
+      for (ix = 0; ix < len; ix++)
       {
         cmd[cix] = buf[ix];
         if (cmd[cix] == '\n')
 	{
           cmd[cix+1] = '\0';
           cix = 0;
-	  SendText (Fd,cmd,0);
+	  SendText(Fd,cmd,0);
         }
 	else if (cix >= MAX_MODULE_INPUT_TEXT_LEN)
 	{
-	  err_msg ("command too long");
+	  err_msg("command too long");
 	  cix = 0;
 	}
 	else
@@ -274,94 +259,111 @@ void close_pipes(void)
   if (!is_closed)
   {
     is_closed = 1;
-    close (Fd[0]);
-    close (Fd[1]);
+    close(Fd[0]);
+    close(Fd[1]);
     close_fifos();
   }
 }
 
 void close_fifos(void)
 {
-  close (Ffdw);
-  close (Ffdr);
-  strcat (F_name,"C");
-  unlink (F_name);
-  F_name[strlen(F_name)-1] = 'M';
-  unlink (F_name);
-  F_name[strlen(F_name)-1] = 'R';
-  unlink (F_name);
-  free (F_name);
-  Ffdr = -1;
-  Ffdw = -1;
+  struct stat stat_buf;
+
+  /* only unlink this file if it is the same as FfdC */
+  /* I know there's a race here between the stat and the unlink, if you know
+   * of a way to unlink from a fildes use it here */
+  if ((stat(FfdC_name, &stat_buf) == 0) && (stat_buf.st_ino == FfdC_ino))
+    unlink(FfdC_name);
+  /* only unlink this file if it is the same as FfdM */
+  if ((stat(FfdM_name, &stat_buf) == 0) && (stat_buf.st_ino == FfdM_ino))
+    unlink(FfdM_name);
+  /* construct the name of the lock file and remove it */
+  FfdM_name[strlen(FfdM_name)-1] = 'R';
+  unlink(FfdM_name);
+  close(FfdM);
+  close(FfdC);
+  FfdC = -1;
+  FfdM = -1;
 }
 
 /*
  * open fifos
  */
-int open_fifos (const char *f_stem)
+int open_fifos(const char *f_stem)
 {
-  char *fc_name, *fm_name;
+  struct stat stat_buf;
 
   /* create 2 fifos */
-  fc_name = malloc( strlen(f_stem) + 2 );
-  if (fc_name == NULL)
+  FfdC_name = malloc(strlen(f_stem) + 2);
+  if (FfdC_name == NULL)
   {
     err_msg( "allocating command" );
     return -1;
   }
-  fm_name = malloc( strlen(f_stem) + 2 );
-  if (fm_name == NULL)
+  FfdM_name = malloc(strlen(f_stem) + 2);
+  if (FfdM_name == NULL)
   {
-    err_msg( "allocating message" );
+    err_msg("allocating message");
     return -1;
   }
-  strcpy(fc_name,f_stem);
-  strcpy(fm_name,f_stem);
-  strcat(fc_name, "C");
-  strcat(fm_name, "M");
+  strcpy(FfdC_name, f_stem);
+  strcpy(FfdM_name, f_stem);
+  strcat(FfdC_name, "C");
+  strcat(FfdM_name, "M");
 
-  if( (Ffdw = open(fc_name, O_RDWR | O_NONBLOCK ) ) > 0)
+  /* check to see if another FvwmCommandS is running by trying to talk to it */
+  FfdC = open(FfdC_name, O_RDWR | O_NONBLOCK);
+  /* remove the fifo's if they exist */
+  unlink(FfdM_name);
+  unlink(FfdC_name);
+  /* If a previous FvwmCommandS is lingering tell it to go away
+   * It will check that it owns the fifo's before removing them so no
+   * there is no need to wait before creating our own */
+  if (FfdC > 0)
   {
-    write_f( Ffdw, "#@killme\n", 9);
-    close( Ffdw );
-  }
-
-  unlink( fm_name );
-  unlink( fc_name );
-
-  if( mkfifo( fm_name, S_IRUSR | S_IWUSR ) < 0 )
-  {
-    err_msg( fm_name );
-    return -1;
-  }
-  if( mkfifo( fc_name, S_IRUSR | S_IWUSR ) < 0 )
-  {
-    err_msg( fc_name );
-    return -1;
+    write_f(FfdC, "killme\n", 9);
+    close(FfdC);
   }
 
-  Ffdr = open(fc_name, O_RDWR | O_NONBLOCK | O_TRUNC);
-  if (Ffdr < 0)
+  /* now we can create the fifo's knowing that they don't already exist
+   * and any lingering FvwmCommandS will not share them or remove them */
+  if (mkfifo(FfdM_name, S_IRUSR | S_IWUSR) < 0)
   {
-    err_msg( "opening command fifo" );
+    err_msg(FfdM_name);
     return -1;
   }
-  free(fc_name);
-  Ffdw = open(fm_name, O_RDWR | O_NONBLOCK | O_TRUNC);
-  if (Ffdw < 0)
+  if (mkfifo(FfdC_name, S_IRUSR | S_IWUSR) < 0)
   {
-    err_msg( "opening message fifo" );
+    err_msg(FfdC_name);
     return -1;
   }
-  free(fm_name);
 
-  F_name = malloc (strlen (f_stem) + 2);
-  if (F_name == NULL)
+  if ((FfdM = open(FfdM_name, O_RDWR | O_NONBLOCK)) < 0)
   {
-    err_msg( "allocating name string" );
+    err_msg("opening message fifo");
     return -1;
   }
-  strcpy (F_name, f_stem);
+  if ((FfdC = open(FfdC_name, O_RDWR | O_NONBLOCK)) < 0)
+  {
+    err_msg("opening command fifo");
+    return -1;
+  }
+
+  /* get the inode numbers for use when quiting */
+  if (fstat(FfdC, &stat_buf) != 0)
+  {
+    err_msg("stat()ing command fifo");
+    return -1;
+  }
+  else
+    FfdC_ino = stat_buf.st_ino;
+  if (fstat(FfdM, &stat_buf) != 0)
+  {
+    err_msg("stat()ing message fifo");
+    return -1;
+  }
+  else
+    FfdM_ino = stat_buf.st_ino;
 
   return 0;
 }
@@ -373,12 +375,35 @@ int open_fifos (const char *f_stem)
 
 void process_message(unsigned long type,unsigned long *body)
 {
-  int  msglen;
+  int msglen;
 
-  switch(type)
+  switch (type)
   {
+  case M_ADD_WINDOW:
   case M_CONFIGURE_WINDOW:
-    relay_packet( type, 24*SOL, body );
+    relay_packet(type, 24 * SOL, body);
+    break;
+
+  case M_ICON_LOCATION:
+  case M_ICONIFY:
+    relay_packet(type, 7 * SOL, body);
+    break;
+
+  case M_MINI_ICON:
+    relay_packet(type, 6 * SOL, body);
+    break;
+
+  case M_NEW_PAGE:
+    relay_packet(type, 5 * SOL, body);
+    break;
+
+  case M_NEW_DESK:
+    relay_packet(type, 1 * SOL, body);
+    break;
+
+  case M_END_WINDOWLIST:
+  case M_END_CONFIG_INFO:
+    relay_packet(type, 0 * SOL, body);
     break;
 
   case M_WINDOW_NAME:
@@ -387,60 +412,15 @@ void process_message(unsigned long type,unsigned long *body)
   case M_RES_NAME:
   case M_ICON_FILE:
   case M_DEFAULTICON:
-    msglen = strlen( (char *)&body[3] );
-    relay_packet( type, msglen+1+3*SOL, body );
-    break;
-
-  case M_END_WINDOWLIST:
-  case M_END_CONFIG_INFO:
-    relay_packet( type, 0*SOL, body );
-    break;
-
-  case M_ICON_LOCATION:
-    relay_packet( type, 7*SOL, body );
-    break;
-
   case M_ERROR:
   case M_STRING:
   case M_CONFIG_INFO:
-    msglen = strlen( (char *)&body[3] );
-    relay_packet( type, msglen+1+3*SOL, body );
-    break;
-
-  case M_MINI_ICON:
-    relay_packet( type, 6*SOL, body );
-    break;
-
-  case M_NEW_PAGE:
-    relay_packet( type, 5*SOL, body );
-    break;
-
-  case M_NEW_DESK:
-    relay_packet( type, 1*SOL, body );
-    break;
-
-  case M_ADD_WINDOW:
-    relay_packet( type, 24*SOL, body );
-    break;
-
-  case M_RAISE_WINDOW:
-  case M_LOWER_WINDOW:
-  case M_FOCUS_CHANGE:
-  case M_DESTROY_WINDOW:
-  case M_DEICONIFY:
-  case M_MAP:
-  case M_WINDOWSHADE:
-  case M_DEWINDOWSHADE:
-    relay_packet( type, 4*SOL, body );
-    break;
-
-  case M_ICONIFY:
-    relay_packet( type, 7*SOL, body );
-
+    msglen = strlen((char *)&body[3]);
+    relay_packet(type, msglen + 1 + 3 * SOL, body);
     break;
 
   default:
-    relay_packet( type, 4*SOL, body );
+    relay_packet(type, 4 * SOL, body);
   }
 }
 
@@ -450,13 +430,12 @@ void process_message(unsigned long type,unsigned long *body)
  * print error message on stderr and exit
  */
 
-void err_msg( const char *msg )
+void err_msg(const char *msg)
 {
-  fprintf( stderr, "%s server error in %s, %s\n",
-	   MYNAME, msg, strerror(errno) );
+  fprintf(stderr, "%s server error in %s, %s\n", MYNAME, msg, strerror(errno));
 }
 
-void err_quit( const char *msg )
+void err_quit(const char *msg)
 {
   err_msg(msg);
   close_pipes();
@@ -466,37 +445,37 @@ void err_quit( const char *msg )
 /*
  * relay packet to front-end
  */
-void relay_packet( unsigned long type,
-		   unsigned long length, unsigned long *body)
+void relay_packet(unsigned long type, unsigned long length,
+                  unsigned long *body)
 {
-  write_f( Ffdw, (char*)&type, SOL );
-  write_f( Ffdw, (char*)&length, SOL );
-  write_f( Ffdw, (char*)body, length );
+  write_f(FfdM, (char*)&type, SOL);
+  write_f(FfdM, (char*)&length, SOL);
+  write_f(FfdM, (char*)body, length);
 }
 
 /*
  * write to fifo
  */
-int write_f (int fd, char *p, int len)
+int write_f(int fd, char *p, int len)
 {
-  int    i, n;
+  int i, n;
   struct timeval tv;
-  int    again;
+  int again;
 
   again = 0;
   tv.tv_sec = 0;
   tv.tv_usec = 50000;
 
-  for (i=0; i<len; )
+  for (i = 0; i < len; )
   {
-    n = write (fd, &p[i], len-i);
-    if (n<0)
+    n = write(fd, &p[i], len - i);
+    if (n < 0)
     {
-      if( errno==EINTR)
+      if(errno == EINTR)
       {
 	continue;
       }
-      else if (errno==EAGAIN )
+      else if (errno == EAGAIN)
       {
 	if (again++ < 5)
 	{
@@ -505,7 +484,7 @@ int write_f (int fd, char *p, int len)
 	}
 	return -1; /* give up this message */
       }
-      err_quit ("writing fifo");
+      err_quit("writing fifo");
     }
     i += n;
   }
