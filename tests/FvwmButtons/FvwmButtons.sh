b60d21db73844618c562d4fceaeb603d8c51e49a
dane <dane>
2001-03-10 19:06:28 +0000

    * tests/FvwmButtons/FvwmButtons.sh: More tests. 	* tests/purify/purify.fvwm2rc: Add xbm bitmap as a side pic,  	default icon.

--- tests/FvwmButtons/FvwmButtons.sh
+++ tests/FvwmButtons/FvwmButtons.sh
@@ -26,27 +26,52 @@ FC () {
   $talk "`date` FvwmCommand $*"
 }
 
+# Draw attention to the pointer
+Pointer_Attn () {
+  # Can't use FC here, tracing destroys the effect.
+  x=1
+  while [ $x -lt 40 ] ; do
+   FvwmCommand "WindowId root WarpToWindow $x $x"
+    x=`expr $x + 4`
+  done
+  while [ $x -gt 10 ] ; do
+   FvwmCommand "WindowId root WarpToWindow $x $x"
+    x=`expr $x - 2`
+  done
+}
+
+button_size=32
+
 # Function pushes all the buttons:
 Operate () {
+  Pointer_Attn
   FC 'Next (FButt) WarpToWindow 20p 20p'
-  sleep `expr 1 \* $slow`
-  FC 'FakeClick Depth 4 press 1 wait 3000 release 1'
-  sleep `expr 1 \* $slow`
-  FC 'CursorMove 32p 0'
-  FC 'FakeClick Depth 4 press 1 wait 3000 release 1'
-  sleep `expr 1 \* $slow`
-  FC 'CursorMove 0 32p'
-  FC 'FakeClick Depth 4 press 1 wait 3000 release 1'
-  sleep `expr 1 \* $slow`
-  FC 'CursorMove -32p 0'
-  FC 'FakeClick Depth 4 press 1 wait 3000 release 1'
+  sleep `expr 2 \* $slow`
+  FC 'FakeClick Depth 4 press 1 wait 30 release 1'
+  sleep `expr 2 \* $slow`
+  FC 'CursorMove '"${button_size}"'p 0'
+  FC 'FakeClick Depth 4 press 1 wait 30 release 1'
+  sleep `expr 2 \* $slow`
+  FC 'CursorMove 0 '"${button_size}"'p'
+  FC 'FakeClick Depth 4 press 1 wait 30 release 1'
+  sleep `expr 2 \* $slow`
+  FC 'CursorMove -'"${button_size}"'p 0'
+  FC 'FakeClick Depth 4 press 1 wait 30 release 1'
+  sleep `expr 3 \* $slow`
 }
 
 talk=true
 slow=1
-while getopts dtos arg ; do
+dosome=n
+while getopts dtos12 arg ; do
   case $arg in
-    d) 
+    1) 
+      do1=y
+      dosome=y;;
+    2) 
+      do2=y
+      dosome=y;; 
+   d) 
       debug=echo;;
     t)
       talk=echo;;
@@ -58,56 +83,68 @@ while getopts dtos arg ; do
   esac
 done
 
+# Test 1 issues:
+# UseTitle doesn't put anything in button, button doesn't operate
+# Padding makes indicator disappear
+# Noborder test needs a window with no border.
+
 # Create shortcut function to create a window
 # arg 0, window name
 FC DestroyFunc W
 FC AddToFunc W I Exec xterm -name '\$0' -g 10x10-20000-20000
 
-# Create a button box with 4 panels, one for each direction
-FC destroymoduleconfig FButt: \'*\'
-# Somewhere near the middle of the screen:
-FC '*FButt: Geometry +300+300'
-FC '*FButt: ButtonGeometry 32x32'
-FC '*FButt: Back gray'
-FC '*FButt: Fore white'
-FC '*FButt: BoxSize smart'
-FC '*FButt: Columns 2'
-FC '*FButt: Rows 2'
-FC '*FButt: Font 9x15bold'
-FC '*FButt: (Panel(up,    indicator) bu "W bu")'
-FC '*FButt: (Panel(right, indicator) br "W br")'
-FC '*FButt: (Panel(left,  indicator) bl "W bl")'
-FC '*FButt: (Panel(down,  indicator) bd "W bd")'
-#style("FButt CirculateHit, neverfocus, mousefocusclickraisesoff");
-FC Module FvwmButtons FButt
+if [ "$dosome" = "n" -o "$do1" = "y" ] ; then
+  # Create a button box with 4 panels, one for each direction
+  FC destroymoduleconfig FButt: \'*\'
+  # Somewhere near the middle of the screen:
+  FC '*FButt: Geometry +300+300'
+  FC '*FButt: ButtonGeometry '"${button_size}x${button_size}"
+  FC '*FButt: Back gray'
+  FC '*FButt: Fore white'
+  FC '*FButt: BoxSize smart'
+  FC '*FButt: Columns 2'
+  FC '*FButt: Rows 2'
+  FC '*FButt: Font 9x15bold'
+  FC '*FButt: (Panel(up,    indicator) bu "W bu")'
+  FC '*FButt: (Panel(right, indicator, noborder) br "W br")'
+  FC '*FButt: (Panel(left, UseTitle) bl "W bl")'
+  FC '*FButt: (Panel(down,  indicator, padding 2) bd "W bd")'
+  #style("FButt CirculateHit, neverfocus, mousefocusclickraisesoff");
+  FC Module FvwmButtons FButt
 
-sleep `expr 2 \* $slow`
-Operate
-sleep `expr 2 \* $slow`
-Operate
+  Operate
+  Operate
+  # Need a long delay here, or the window just goes away
+  sleep `expr 2 \* $slow`
+  FC Beep
+  FC KillModule FvwmButtons FButt
+fi
 
-FC KillModule FvwmButtons FButt
 
-# Create a button box with 4 panels, one for each direction
-FC destroymoduleconfig FButt: \'*\'
-FC '*FButt: Geometry +300+300'
-FC '*FButt: ButtonGeometry 32x32'
-FC '*FButt: Back gray'
-FC '*FButt: Fore white'
-FC '*FButt: BoxSize smart'
-FC '*FButt: Columns 2'
-FC '*FButt: Rows 2'
-FC '*FButt: Font 9x15bold'
-FC '*FButt: (Panel(up,    indicator, delay 3000, smooth) bu "W bu")'
-FC '*FButt: (Panel(right, indicator, hints)              br "W br")'
-FC '*FButt: (Panel(left,  indicator 16) bl "W bl")'
-FC '*FButt: (Panel(down,  indicator, steps 1) bd "W bd")'
-FC Module FvwmButtons FButt
+# The "down" button with 1 step doesn't draw anything.
 
-sleep `expr 2 \* $slow`
-Operate
-sleep `expr 2 \* $slow`
-Operate
+if [ "$dosome" = "n" -o "$do2" = "y" ] ; then
+  button_size=64
+  # Create a button box with 4 panels, one for each direction
+  FC destroymoduleconfig FButt: \'*\'
+  FC '*FButt: Geometry +400+400'
+  FC '*FButt: ButtonGeometry '"${button_size}x${button_size}"
+  FC '*FButt: Back cornflowerblue'
+  FC '*FButt: Fore white'
+  FC '*FButt: BoxSize fixed'
+  FC '*FButt: Columns 2'
+  FC '*FButt: Rows 2'
+  FC '*FButt: Font 9x15bold'
+  FC '*FButt: (Panel(up,    indicator, delay 3000, smooth, steps 3) bu "W bu")'
+  FC '*FButt: (Panel(right, indicator, hints)              br "W br")'
+  FC '*FButt: (Panel(left,  indicator 16) bl "W bl")'
+  FC '*FButt: (Panel(down,  indicator, steps 1) bd "W bd")'
+  FC Module FvwmButtons FButt
 
-FC KillModule FvwmButtons FButt
-
+  Operate
+  Operate
+  # Need a long delay here, or the window just goes away
+  sleep `expr 2 \* $slow`
+  FC Beep
+  FC KillModule FvwmButtons FButt
+fi
