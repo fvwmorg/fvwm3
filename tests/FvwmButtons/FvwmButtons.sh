e46167079979b2924c12361d11a9a62b5f111f0b
domivogt <domivogt>
2001-03-10 20:50:38 +0000

    * Some enhancements to FvwmButtons test script. * tests directory has its own ChangeLog.

--- tests/FvwmButtons/FvwmButtons.sh
+++ tests/FvwmButtons/FvwmButtons.sh
@@ -10,8 +10,11 @@
 
 # Args are:
 # -d debug by echoing commands instead of issuing them.
+# -k do not kill FvwmButtons if script is interrupted with ctrl-c
 # -t trace exection
 # -s run slowly.  Needed on very slow connections.
+# -S specify a delay between the test steps. -S 1 is the default, -S 20 is
+#    equivalent to -s
 # -o just operate the 4 button setup once.
 
 #  Modification History
@@ -19,6 +22,10 @@
 #  Created on 03/02/01 by Dan Espen (dane):
 #  - Tests for FvwmButtons.
 
+
+# Base unit in seconds for delay
+DEFAULT_DELAY=1
+DEFAULT_SLOWNESS_FACTOR=20
 
 # Function for running an Fvwm command:
 FC () {
@@ -40,48 +47,72 @@ Pointer_Attn () {
   done
 }
 
+trap_sigint () {
+  FC KillModule FvwmButtons FButt
+  exit 0
+}
+
 button_size=32
+
+# Function to insert a pause
+delay () {
+  if [ x$1 = x ] ; then
+    DELAY=$DEFAULT_DELAY
+  else
+    DELAY=$1
+  fi
+  sleep `expr $DELAY \* $slow`
+}
 
 # Function pushes all the buttons:
 Operate () {
   Pointer_Attn
   FC 'Next (FButt) WarpToWindow 20p 20p'
-  sleep `expr 2 \* $slow`
+  delay
   FC 'FakeClick Depth 4 press 1 wait 30 release 1'
-  sleep `expr 2 \* $slow`
+  delay
   FC 'CursorMove '"${button_size}"'p 0'
   FC 'FakeClick Depth 4 press 1 wait 30 release 1'
-  sleep `expr 2 \* $slow`
+  delay
   FC 'CursorMove 0 '"${button_size}"'p'
   FC 'FakeClick Depth 4 press 1 wait 30 release 1'
-  sleep `expr 2 \* $slow`
+  delay
   FC 'CursorMove -'"${button_size}"'p 0'
   FC 'FakeClick Depth 4 press 1 wait 30 release 1'
-  sleep `expr 3 \* $slow`
+  delay
 }
 
 talk=true
 slow=1
 dosome=n
-while getopts dtos12 arg ; do
+kill=1
+while getopts dkosS:t12 arg ; do
   case $arg in
-    1) 
+    1)
       do1=y
       dosome=y;;
-    2) 
+    2)
       do2=y
-      dosome=y;; 
-   d) 
+      dosome=y;;
+    d)
       debug=echo;;
+    k)
+      kill=0;;
+    o)
+      Operate
+      exit 0;;
+    s)
+      slow=$DEFAULT_SLOWNESS_FACTOR;;
+    S)
+      slow=$OPTARG;;
     t)
       talk=echo;;
-    s)
-      slow=10;;
-    o)
-     Operate
-     exit 0;;
   esac
 done
+
+if [ $kill = 1 ] ; then
+  trap trap_sigint 2;;
+fi
 
 # Test 1 issues:
 # UseTitle doesn't put anything in button, button doesn't operate
@@ -92,6 +123,9 @@ done
 # arg 0, window name
 FC DestroyFunc W
 FC AddToFunc W I Exec xterm -name '\$0' -g 10x10-20000-20000
+
+# Setup window style for FvwmButtons
+FC Style FButt CirculateHit, neverfocus, mousefocusclickraisesoff
 
 if [ "$dosome" = "n" -o "$do1" = "y" ] ; then
   # Create a button box with 4 panels, one for each direction
@@ -108,20 +142,17 @@ if [ "$dosome" = "n" -o "$do1" = "y" ] ; then
   FC '*FButt: (Panel(up,    indicator) bu "W bu")'
   FC '*FButt: (Panel(right, indicator, noborder) br "W br")'
   FC '*FButt: (Panel(left, UseTitle) bl "W bl")'
-  FC '*FButt: (Panel(down,  indicator, padding 2) bd "W bd")'
-  #style("FButt CirculateHit, neverfocus, mousefocusclickraisesoff");
+  FC '*FButt: (Padding 2, Panel(down,  indicator) bd "W bd")'
   FC Module FvwmButtons FButt
 
   Operate
   Operate
   # Need a long delay here, or the window just goes away
-  sleep `expr 2 \* $slow`
+  delay
   FC Beep
   FC KillModule FvwmButtons FButt
 fi
 
-
-# The "down" button with 1 step doesn't draw anything.
 
 if [ "$dosome" = "n" -o "$do2" = "y" ] ; then
   button_size=64
@@ -135,7 +166,7 @@ if [ "$dosome" = "n" -o "$do2" = "y" ] ; then
   FC '*FButt: Columns 2'
   FC '*FButt: Rows 2'
   FC '*FButt: Font 9x15bold'
-  FC '*FButt: (Panel(up,    indicator, delay 3000, smooth, steps 3) bu "W bu")'
+  FC '*FButt: (Panel(up,    indicator, delay 250, smooth, steps 3) bu "W bu")'
   FC '*FButt: (Panel(right, indicator, hints)              br "W br")'
   FC '*FButt: (Panel(left,  indicator 16) bl "W bl")'
   FC '*FButt: (Panel(down,  indicator, steps 1) bd "W bd")'
@@ -144,7 +175,7 @@ if [ "$dosome" = "n" -o "$do2" = "y" ] ; then
   Operate
   Operate
   # Need a long delay here, or the window just goes away
-  sleep `expr 2 \* $slow`
+  delay
   FC Beep
   FC KillModule FvwmButtons FButt
 fi
