107115c23d013ad37f40f7d452ce4e07df1966a7
domivogt <domivogt>
2001-03-24 10:51:23 +0000

    * SOme improvements to conversion script. * Man page rewording.

--- utils/configure_dev.sh
+++ utils/configure_dev.sh
@@ -3,7 +3,7 @@
 # reconfigure CVS source code
 # must be called from main fvwm directory
 echo automake
-automake || exit 1
+automake --add-missing || exit 1
 echo autoreconf
 autoreconf || exit 2
 echo ./configure
