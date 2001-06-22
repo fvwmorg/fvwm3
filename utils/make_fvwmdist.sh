64bb67aeeedfc32226211a13ee16cb6eef904048
domivogt <domivogt>
2001-06-22 21:59:36 +0000

    * Improved build instructions.

--- utils/make_fvwmdist.sh
+++ utils/make_fvwmdist.sh
@@ -222,6 +222,7 @@ fi
 echo " . Upload the distribution to ftp://ftp.fvwm.org/pub/incoming/fvwm"
 echo " . Notify fvwm-owner@fvwm.org of the upload"
 echo " . Update the version numbers in fvwm-web/download.html, docs/FAQ"
+echo "   and fvwm-web/index.html."
 echo " . If releasing the stable branch, update NEWS in the beta branch to"
 echo "   identify the latest stable release and describe the changes."
 echo " . Use fvwm-web generated/txt2html.sh to update the NEWS file."
