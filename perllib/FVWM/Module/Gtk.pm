3b6ba204b82b24424ca62333438ce955f608da2c
migo <migo>
2002-02-25 02:34:59 +0000

    * new subclass in perl library for writting Perl/Gtk based FVWM modules _ extra Gtk perl library from CPAN is needed to run new example FVWM module
    Since the word "module" has 2 meanings on this subject:
    	FVWM module == executable
    	perl module == .pm file
    I will try in discussions to use the word "module" only for the first and
    "class in perl library" for the second.

--- perllib/FVWM/Module/Gtk.pm
+++ perllib/FVWM/Module/Gtk.pm
@@ -12,7 +12,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-package FVWM::Module::Tk;
+package FVWM::Module::Gtk;
 
 require 5.003;
 
@@ -21,45 +21,30 @@ use vars qw($VERSION @ISA @EXPORT);
 use Exporter;
 
 use FVWM::Module;
-use Tk;
+use Gtk;
 
 @ISA = qw(FVWM::Module Exporter);
 @EXPORT = @FVWM::Module::EXPORT;
 
 $VERSION = "1.1";
 
-sub new ($$@) {
-	my $class = shift;
-	$class = ref($class) || $class;
-	my $top = shift;
-	my %params = @_;
-
-	my $self = $class->SUPER::new(%params);
-	bless $self, $class;
-
-	$self->internalDie("No Tk::Toplevel given in constructor")
-		unless UNIVERSAL::isa($top, "Tk::Toplevel");
-
-	$self->{topLevel} = $top;
-	$self->{winId} = $top->id;
-
-	return $self;
-}
-
 sub eventLoop ($) {
 	my $self = shift;
 
-	my $top = $self->{topLevel};
-	$top->fileevent($self->{istream},
-		readable => sub {
+	Gtk::Gdk->input_add(
+		$self->{istream}->fileno, ['read'],
+		sub ($$$) {
+			#my ($socket, $fd, $flags) = @_;
+			#return 0 unless $flags->{'read'};
 			unless ($self->processPacket($self->readPacket)) {
-				$self->invokeHandler(ON_EXIT);
-				$self->destroy;
-				$top->destroy;
+				Gtk->main_quit;
 			}
+			return 1;
 		}
 	);
-	MainLoop;
+	Gtk->main;
+	$self->invokeHandler(ON_EXIT);
+	$self->destroy;
 }
 
 sub addDefaultErrorHandler ($) {
@@ -68,27 +53,23 @@ sub addDefaultErrorHandler ($) {
 	$self->addHandler(M_ERROR, sub {
 		my ($self, $type, @args) = @_;
 
-		my $top = $self->{topLevel};
+		my $dialog = new Gtk::Dialog;
+		$dialog->set_title("FVWM Error");
+		$dialog->set_border_width(4);
 
-		my $err = $top->Dialog(
-			-title => 'FVWM Error',
-			-bitmap => 'error',
-			-default_button => 'Dismiss',
-			-text => $args[3],
-			-buttons => ['Dismiss', 'Exit']
-		);
-		my $btn = $err->Show(-global);
+		my $label = new Gtk::Label $args[3];
+		$dialog->vbox->pack_start($label, 0, 1, 10);
 
-		die "quit" if $btn eq 'Exit';
+		my $button = new Gtk::Button "Dismiss";
+		$dialog->action_area->pack_start($button, 1, 1, 0);
+		$button->signal_connect("clicked", sub { $dialog->destroy; });
+
+		$button = new Gtk::Button "Exit";
+		$dialog->action_area->pack_start($button, 1, 1, 0);
+		$button->signal_connect("clicked", sub { Gtk->main_quit; });
+
+		$dialog->show_all;
 	});
-}
-
-sub topLevel ($) {
-	return shift->{topLevel};
-}
-
-sub winId ($) {
-	return shift->{winId};
 }
 
 1;
@@ -97,15 +78,20 @@ __END__
 
 =head1 NAME
 
-FVWM::Module::Tk - FVWM::Module with the Tk widget library attached
+FVWM::Module::Gtk - FVWM::Module with the GTK+ widget library attached
 
 =head1 SYNOPSIS
 
-  use Tk;
-  use FVWM::Module::Tk;
+  use Gtk;
+  use FVWM::Module::Gtk;
 
-  my $top = new MainWindow;
-  my $module = new FVWM::Module::Tk $top;
+  my $module = new FVWM::Module::Gtk;
+
+  init Gtk;
+  my $window = new Gtk::Window -toplevel;;
+  my $label = new Gtk::Label "Hello, world";
+  $window->add($label);
+  $window->show_all;
 
   $module->addHandler(M_CONFIGURE_WINDOW, \&configure_toplevel);
   $module->addHandler(M_CONFIG_INFO, \&some_other_sub);
@@ -114,9 +100,9 @@ FVWM::Module::Tk - FVWM::Module with the Tk widget library attached
 
 =head1 DESCRIPTION
 
-The B<FVWM::Module::Tk> package is a sub-class of B<FVWM::Module> that
-overloads the methods B<new>, B<eventLoop> and B<addDefaultErrorHandler>
-to manage Tk objects as well.
+The B<FVWM::Module::Gtk> package is a sub-class of B<FVWM::Module> that
+overloads the methods B<eventLoop> and B<addDefaultErrorHandler> to manage
+GTK+ objects as well.
 
 This manual page details only those differences. For details on the
 API itself, see L<FVWM::Module>.
@@ -128,40 +114,20 @@ are covered here:
 
 =over 8
 
-=item new
-
-$self = new FVWM::Module::Tk $top, %params
-
-Create and return an object of the B<FVWM::Module::Tk> class. The return value is
-the blessed reference. This B<new> method is identical to the parent class
-method, with the exception that a Tk top-level of some sort (MainWindow,
-TopLevel, Frame, etc.) must be passed before the hash of options. The options
-themselves are as specified in L<FVWM::Module>.
-
 =item eventLoop 
 
 From outward appearances, this methods operates just as the parent
 B<eventLoop> does. It is worth mentioning, however, that this version
-enters into the Tk B<MainLoop> subroutine, ostensibly not to return.
+enters into the B<Gtk->main> subroutine, ostensibly not to return.
 
 =item addDefaultErrorHandler
 
-This methods adds a M_ERROR handler that creates a dialog box using the Tk
+This methods adds a M_ERROR handler that creates a dialog box using the GTK+
 widgets to notify you that an error has been reported by FVWM. The dialog
 has two buttons, labelled "Exit" and "Dismiss". Selecting the "Dismiss"
-button closes the dialog, and allows your application to continue (the
-handler grabs the pointer until the dialog is closed). Choosing the "Exit"
-button causes the termination of the running module. After exiting that
-window, the application will continue as if the "Dismiss" button had been
-pressed.
-
-=item topLevel
-
-Returns the Tk toplevel that this object was created with.
-
-=item winId
-
-A shortcut for C<$self->topLevel->id>, exists for efficiency reasons.
+button closes the dialog. Choosing the "Exit" button causes the termination
+of the running module. After exiting that window, the application will
+continue as if the "Dismiss" button had been pressed.
 
 =back
 
@@ -181,11 +147,11 @@ flexibility, not bugs.
 
 Mikhael Goikhman <migo@homemail.com>.
 
-Randy J. Ray <randy@byz.org>, original B<X11::Fvwm> and B<X11::Fvwm::Tk>.
-
 =head1 THANKS TO
 
-Nick Ing-Simmons <Nick.Ing-Simmons@tiuk.ti.com> for Tk Perl extension.
+Randy J. Ray <randy@byz.org>.
+
+Kenneth Albanowski, Paolo Molaro for Gtk/Perl extension.
 
 =head1 SEE ALSO
 
