c3af50b03b5551eebb9957a6989ceff11f986e48
dane <dane>
2002-03-09 03:14:19 +0000

    * FvwmForm/FvwmForm.c (PutDataInForm): Remove incorrect comment.

--- modules/FvwmForm/FvwmForm.c
+++ modules/FvwmForm/FvwmForm.c
@@ -931,7 +931,6 @@ static void PutDataInForm(char *cp)
         item->input.buf = var_len+1;
         item->input.value = safemalloc(item->input.buf);
         strcpy(item->input.value,cp);     /* new value in field */
-        /* New value, but don't change length */
         free(var_name);                 /* goto's have their uses */
         return;
       }
