f074e8a50cd584a159f764a57eb3c1bb3ac0f53e
domivogt <domivogt>
2002-07-22 09:06:45 +0000

    * Fixed alloca() leak in the modules:  call alloca(0) in fvwmSelect.

--- libs/fvwmsignal.c
+++ libs/fvwmsignal.c
@@ -39,7 +39,7 @@ static int term_sigs;
 void
 fvwmSetSignalMask(int sigmask)
 {
-  term_sigs = sigmask;
+	term_sigs = sigmask;
 }
 
 
@@ -52,7 +52,7 @@ fvwmSetSignalMask(int sigmask)
 int
 fvwmGetSignalMask(void)
 {
-  return term_sigs;
+	return term_sigs;
 }
 
 #endif
@@ -70,29 +70,29 @@ fvwmGetSignalMask(void)
 void
 fvwmSetTerminate(int sig)
 {
-  BSD_BLOCK_SIGNALS;
+	BSD_BLOCK_SIGNALS;
 
-  isTerminated = true;
+	isTerminated = true;
 #ifdef DEBUG
-  debug_term_signal = sig;
+	debug_term_signal = sig;
 #endif
 
-  if (canJump)
-  {
-    canJump = false;
+	if (canJump)
+	{
+		canJump = false;
 
-    /*
-     * This non-local jump is safe ONLY because we haven't called
-     * any non-reentrant functions in the short period where the
-     * "canJump" variable is true.
-     *
-     * NOTE: No need to restore the signal mask, since siglongjmp
-     *       is designed to do that for us.
-     */
-    siglongjmp(deadJump, SIG_DONE);
-  }
+		/*
+		 * This non-local jump is safe ONLY because we haven't called
+		 * any non-reentrant functions in the short period where the
+		 * "canJump" variable is true.
+		 *
+		 * NOTE: No need to restore the signal mask, since siglongjmp
+		 *       is designed to do that for us.
+		 */
+		siglongjmp(deadJump, SIG_DONE);
+	}
 
-  BSD_UNBLOCK_SIGNALS;
+	BSD_UNBLOCK_SIGNALS;
 }
 
 
@@ -108,67 +108,73 @@ fvwmSelect(fd_set_size_t nfds,
 	   fd_set *readfds, fd_set *writefds, fd_set *exceptfds,
 	   struct timeval *timeout)
 {
-  volatile int iRet = -1;  /* This variable MUST NOT be in a register */
+	volatile int iRet = -1;  /* This variable MUST NOT be in a register */
 
-  /*
-   * Yes, we trash errno here, but you're only supposed to check
-   * errno immediately after a function fails anyway. If we fail,
-   * then it's because we received a signal. If we succeed, we
-   * shouldn't be checking errno. And if somebody calls us expecting
-   * us to preserve errno then that's their bug.
-   *
-   * NOTE: We mustn't call any function that might trash errno
-   *       ourselves, except select() itself of course. I believe
-   *       that sigsetjmp() does NOT trash errno.
-   */
-  errno = EINTR;
+#ifdef C_ALLOCA
+	/* If we're using the C version of alloca, see if anything needs to be
+	 * freed up.
+	 */
+	alloca(0);
+#endif
 
-  /*
-   * Now initialise the non-local jump. Between here and the end of
-   * the routine (more-or-less) we must NOT call any non-reentrant
-   * functions! This is because we might need to abandon them half
-   * way through execution and return here!
-   */
-  if ( sigsetjmp(deadJump, 1) == SIG_INIT )
-  {
-    /*
-     * Activate the non-local jump. Between now and when we turn the
-     * jump off again, we must NOT call any non-reentrant functions
-     * because we could be interrupted halfway through ...
-     */
-    canJump = true;
+	/*
+	 * Yes, we trash errno here, but you're only supposed to check
+	 * errno immediately after a function fails anyway. If we fail,
+	 * then it's because we received a signal. If we succeed, we
+	 * shouldn't be checking errno. And if somebody calls us expecting
+	 * us to preserve errno then that's their bug.
+	 *
+	 * NOTE: We mustn't call any function that might trash errno
+	 *       ourselves, except select() itself of course. I believe
+	 *       that sigsetjmp() does NOT trash errno.
+	 */
+	errno = EINTR;
 
-    /*
-     * If we have already been told to terminate then we will not
-     * execute the select() because the flag will be set. If a
-     * "terminate" signal arrives between testing the flag and
-     * calling select() then we will jump back to the non-local
-     * jump point ...
-     */
-    if ( !isTerminated )
-    {
-      /*
-       * The "die" signal will interrupt this system call:
-       * that IS the whole point, after all :-)
-       */
-      iRet = select(nfds,
-		    SELECT_FD_SET_CAST readfds,
-		    SELECT_FD_SET_CAST writefds,
-		    SELECT_FD_SET_CAST exceptfds,
-		    timeout);
-    }
+	/*
+	 * Now initialise the non-local jump. Between here and the end of
+	 * the routine (more-or-less) we must NOT call any non-reentrant
+	 * functions! This is because we might need to abandon them half
+	 * way through execution and return here!
+	 */
+	if ( sigsetjmp(deadJump, 1) == SIG_INIT )
+	{
+		/*
+		 * Activate the non-local jump. Between now and when we turn the
+		 * jump off again, we must NOT call any non-reentrant functions
+		 * because we could be interrupted halfway through ...
+		 */
+		canJump = true;
 
-    /*
-     * The non-local jump is about to go out of scope,
-     * so we must deactivate it. Note that the return-
-     * value from select() will be safely stored in the
-     * local variable before the jump is disabled.
-     */
-    canJump = false;
-  }
+		/*
+		 * If we have already been told to terminate then we will not
+		 * execute the select() because the flag will be set. If a
+		 * "terminate" signal arrives between testing the flag and
+		 * calling select() then we will jump back to the non-local
+		 * jump point ...
+		 */
+		if ( !isTerminated )
+		{
+			/*
+			 * The "die" signal will interrupt this system call:
+			 * that IS the whole point, after all :-)
+			 */
+			iRet = select(nfds,
+				      SELECT_FD_SET_CAST readfds,
+				      SELECT_FD_SET_CAST writefds,
+				      SELECT_FD_SET_CAST exceptfds,
+				      timeout);
+		}
 
-  return iRet;
+		/*
+		 * The non-local jump is about to go out of scope,
+		 * so we must deactivate it. Note that the return-
+		 * value from select() will be safely stored in the
+		 * local variable before the jump is disabled.
+		 */
+		canJump = false;
+	}
+
+	return iRet;
 }
 
 #endif  /* HAVE_SELECT */
-
