20f2a844c2882d77b8eff8ec4405b0541b3a6adc
domivogt <domivogt>
2002-09-13 14:34:51 +0000

    * Added autoheader call to configure_dev.sh.

--- utils/configure_dev.sh
+++ utils/configure_dev.sh
@@ -4,9 +4,11 @@
 # must be called from main fvwm directory
 echo aclocal
 aclocal || exit 1
+echo autoheader
+autoheader || exit 2
 echo automake
-automake --add-missing || exit 2
+automake --add-missing || exit 3
 echo autoreconf
-autoreconf || exit 3
+autoreconf || exit 4
 echo ./configure "$@"
-./configure "$@" || exit 4
+./configure "$@" || exit 5
