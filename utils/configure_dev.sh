0d0b38417b1b89158ab80484800be1e130c5b32c
dane <dane>
2003-04-16 01:51:57 +0000

    * configure_dev.sh: Nice streamlining from Cameron Simpson.

--- utils/configure_dev.sh
+++ utils/configure_dev.sh
@@ -2,13 +2,9 @@
 
 # reconfigure CVS source code
 # must be called from main fvwm directory
-echo aclocal
+set -x
 aclocal || exit 1
-echo autoheader
 autoheader || exit 2
-echo automake
 automake --add-missing || exit 3
-echo autoreconf
 autoreconf || exit 4
-echo ./configure "$@"
-./configure "$@" || exit 5
+./configure ${1+"$@"} || exit 5
