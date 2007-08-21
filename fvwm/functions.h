73f9db7760671473dd0fd4037e429aff4c6ac97e
domivogt <domivogt>
2007-08-21 09:26:40 +0000

    * Added constants for command group tagging.

--- fvwm/functions.h
+++ fvwm/functions.h
@@ -20,7 +20,31 @@ typedef enum
 	/* only to be passed to execute_function() */
 	FUNC_IS_UNMANAGED        = 0x20,
 	FUNC_DONT_EXPAND_COMMAND = 0x40,
-	FUNC_DONT_DEFER          = 0x80
+	FUNC_DONT_DEFER          = 0x80,
+
+	/* The values are not used internally but by external scripts parsing
+	 * functable.  Hence all the values below are 0
+	 */
+	/* tagging used only for building the documentation */
+	FUNC_OBSOLETE            = 0,
+	FUNC_DEPRECATED          = 0,
+	/* command grouping (used only for building the documentation) */
+	/*!!!*/
+	FG_BINDING               = 0,
+	FG_MODULE                = 0,
+	FG_MENU                  = 0,
+	FG_SESSION               = 0,
+	FG_STYLE                 = 0,
+	FG_MOVE                  = 0,
+	FG_STATE                 = 0,
+	FG_COND                  = 0,
+	FG_USER                  = 0,
+	FG_COLOR                 = 0,
+	FG_EWMH_GNOME            = 0,
+	FG_VIRTUAL               = 0,
+	FG_FOCUS                 = 0,
+	FG_MISC                  = 0,
+	FG_OLD                   = 0
 } execute_flags_t;
 
 /* ---------------------------- global macros ------------------------------ */
