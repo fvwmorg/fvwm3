2791dd5db29d01cf45af2329534dc3094e93f16d
Thomas Adam <thomas@fvwm.org>
2019-05-26 23:59:14 +0100

    compat: add asprintf implementation
    For those systems which don't have asprintf natively, provide a
    compatibility implementation.

---
+++ libs/asprintf.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2006 Nicholas Marriott <nicholas.marriott@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "config.h"
+#include "safemalloc.h"
+
+#include <sys/types.h>
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+asprintf(char **ret, const char *fmt, ...)
+{
+	va_list	ap;
+	int	n;
+
+	va_start(ap, fmt);
+	n = vasprintf(ret, fmt, ap);
+	va_end(ap);
+
+	return (n);
+}
+
+int
+vasprintf(char **ret, const char *fmt, va_list ap)
+{
+	int	 n;
+	va_list  ap2;
+
+	va_copy(ap2, ap);
+
+	if ((n = vsnprintf(NULL, 0, fmt, ap)) < 0)
+		goto error;
+
+	*ret = xmalloc(n + 1);
+	if ((n = vsnprintf(*ret, n + 1, fmt, ap2)) < 0) {
+		free(*ret);
+		goto error;
+	}
+	va_end(ap2);
+
+	return (n);
+
+error:
+	va_end(ap2);
+	*ret = NULL;
+	return (-1);
+}
