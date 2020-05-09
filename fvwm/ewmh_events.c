ThomasAdam/mvwm

ewmhInfo: move into struct monitor, update callers
Remove the extern ewmhc definition, moving it into struct monitor so that it can
be referenced on a per-monitor basis.

In doing this, and because certain callers assumed ewmhc was global, rework them
to get the values out the appropriate monitor.

mvwm/ewmh_events.c
@@ -135,27 +135,30 @@ int ewmh_NumberOfDesktops(EWMH_CMD_ARGS)
 	struct monitor	*m;
 
 	/* not a lot of sinification for mvwm */
-	if (d > 0 && (d <= ewmhc.MaxDesktops || ewmhc.MaxDesktops == 0))
-	{
-		ewmhc.NumberOfDesktops = d;
-		TAILQ_FOREACH(m, &monitor_q, entry) {
-			if (monitor_should_ignore_global(m))
-				continue;
-			EWMH_SetNumberOfDesktops(m);
+	TAILQ_FOREACH(m, &monitor_q, entry) {
+		if (monitor_should_ignore_global(m))
+			continue;
+
+		EWMH_SetNumberOfDesktops(m);
+
+		if (d > 0 &&
+		    (d <= m->ewmhc.MaxDesktops || m->ewmhc.MaxDesktops == 0))
+		{
+			m->ewmhc.NumberOfDesktops = d;
 		}
-	}
-	else
-	{
-		mvwm_msg(
-			WARN, "ewmh_NumberOfDesktops",
-			"The application window (id %#lx)\n"
-			"  \"%s\" tried to set an invalid number of desktops"
-			" (%ld)\n"
-			"  using an EWMH client message.\n"
-			"    mvwm is ignoring this request.\n",
-			fw ? FW_W(fw) : 0, fw ? fw->name.name : "(none)",
-			ev->xclient.data.l[0]);
-		mvwm_msg_report_app_and_workers();
+		else
+		{
+			mvwm_msg(
+				WARN, "ewmh_NumberOfDesktops",
+				"The application window (id %#lx)\n"
+				"  \"%s\" tried to set an invalid number of desktops"
+				" (%ld)\n"
+				"  using an EWMH client message.\n"
+				"    mvwm is ignoring this request.\n",
+				fw ? FW_W(fw) : 0, fw ? fw->name.name : "(none)",
+				ev->xclient.data.l[0]);
+			mvwm_msg_report_app_and_workers();
+		}
 	}
 
 	return -1;

libs/FScreen.h
@@ -2,10 +2,14 @@
 #ifndef MVWMLIB_FSCRREN_H
 #define MVWMLIB_FSCRREN_H
 
+/* For CARD32 */
+#include <X11/Xproto.h>
+
 #include "mvwm/mvwm.h"
 #include "mvwm/execcontext.h"
 #include "mvwm/misc.h"
 #include "mvwm/screen.h"
+#include "mvwm/ewmh_intern.h"
 
 typedef union
 {
@@ -40,6 +44,9 @@ struct monitor {
 	 * correct for any desktop not in the list
 	 */
 	DesktopsInfo	*Desktops;
+
+	/* Information about EWMH */
+	ewmhInfo ewmhc;
 
 	struct {
 		int VxMax;

fvwmorg/fvwm3

fvwm/ewmh_events.c
@@ -136,26 +136,28 @@ int ewmh_NumberOfDesktops(
 	int d = ev->xclient.data.l[0];
 	struct monitor	*m;
 
-	m = (fw && fw->m) ? fw->m : monitor_get_current();
-
 	/* not a lot of sinification for fvwm */
-	if (d > 0 && (d <= ewmhc.MaxDesktops || ewmhc.MaxDesktops == 0))
-	{
-		ewmhc.NumberOfDesktops = d;
+	TAILQ_FOREACH(m, &monitor_q, entry) {
 		EWMH_SetNumberOfDesktops(m);
-	}
-	else
-	{
-		fvwm_msg(
-			WARN, "ewmh_NumberOfDesktops",
-			"The application window (id %#lx)\n"
-			"  \"%s\" tried to set an invalid number of desktops"
-			" (%ld)\n"
-			"  using an EWMH client message.\n"
-			"    fvwm is ignoring this request.\n",
-			fw ? FW_W(fw) : 0, fw ? fw->name.name : "(none)",
-			ev->xclient.data.l[0]);
-		fvwm_msg_report_app_and_workers();
+
+		if (d > 0 &&
+		    (d <= m->ewmhc.MaxDesktops || m->ewmhc.MaxDesktops == 0))
+		{
+			m->ewmhc.NumberOfDesktops = d;
+		}
+		else
+		{
+			mvwm_msg(
+				WARN, "ewmh_NumberOfDesktops",
+				"The application window (id %#lx)\n"
+				"  \"%s\" tried to set an invalid number of desktops"
+				" (%ld)\n"
+				"  using an EWMH client message.\n"
+				"    fvwm is ignoring this request.\n",
+				fw ? FW_W(fw) : 0, fw ? fw->name.name : "(none)",
+				ev->xclient.data.l[0]);
+			fvwm_msg_report_app_and_workers();
+		}
 	}
 
 	return -1;

libs/FScreen.h
@@ -6,6 +6,15 @@
 #include <X11/extensions/Xrandr.h>
 #endif
 
+/* For CARD32 */
+#include <X11/Xproto.h>
+
+#include "fvwm/fvwm.h"
+#include "fvwm/execcontext.h"
+#include "fvwm/misc.h"
+#include "fvwm/screen.h"
+#include "fvwm/ewmh_intern.h"
+
 /* needs X11/Xlib.h and X11/Xutil.h */
 
 typedef struct
@@ -101,6 +110,9 @@ struct monitor {
          * correct for any desktop not in the list
          */
 	DesktopsInfo    *Desktops;
+
+	/* Information about EWMH */
+	ewmhInfo ewmhc;
 
         /* Information about EWMH. */
         struct {