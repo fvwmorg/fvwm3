ThomasAdam/mvwm

ewmhInfo: move into struct monitor, update callers
Remove the extern ewmhc definition, moving it into struct monitor so that it can
be referenced on a per-monitor basis.

In doing this, and because certain callers assumed ewmhc was global, rework them
to get the values out the appropriate monitor.

libs/FScreen.h
@@ -2,10 +2,14 @@
 #ifndef MVWMLIB_FSCRREN_H
 #define MVWMLIB_FSCRREN_H
 
+/* For CARD32 */
+#include <X11/Xproto.h>
+
 #include "mvwm/mvwm.h"
 #include "mvwm/execcontext.h"
 #include "mvwm/misc.h"
 #include "mvwm/screen.h"
+#include "mvwm/ewmh_intern.h"
 
 typedef union
 {
@@ -40,6 +44,9 @@ struct monitor {
 	 * correct for any desktop not in the list
 	 */
 	DesktopsInfo	*Desktops;
+
+	/* Information about EWMH */
+	ewmhInfo ewmhc;
 
 	struct {
 		int VxMax;

fvwmorg/fvwm3

libs/FScreen.h
@@ -6,6 +6,15 @@
 #include <X11/extensions/Xrandr.h>
 #endif
 
+/* For CARD32 */
+#include <X11/Xproto.h>
+
+#include "fvwm/fvwm.h"
+#include "fvwm/execcontext.h"
+#include "fvwm/misc.h"
+#include "fvwm/screen.h"
+#include "fvwm/ewmh_intern.h"
+
 /* needs X11/Xlib.h and X11/Xutil.h */
 
 typedef struct
@@ -100,6 +109,9 @@ struct monitor {
          * correct for any desktop not in the list
          */
 	DesktopsInfo    *Desktops;
+
+	/* Information about EWMH */
+	ewmhInfo ewmhc;
 
         /* Information about EWMH. */
         struct {