e86f62d20441fa6f07736c7c557462ff20769fbb
Dominik Vogt <dominik.vogt@gmx.de>
2021-11-15 02:03:44 +0100

    Remove Efence and Dmalloc support
    These were useful once before more mature/established tools such as
    valgrind came along.

--- libs/fvwmlib.h
+++ libs/fvwmlib.h
@@ -12,11 +12,6 @@
 /* Convenience function ti init all the graphics subsystems */
 void flib_init_graphics(Display *dpy);
 
-/* Set up heap debugging library dmalloc.  */
-#ifdef HAVE_DMALLOC_H
-#include <dmalloc.h>
-#endif
-
 /* Set up mtrace from glibc 2.1.x for x > ?  */
 #ifdef MTRACE_DEBUGGING
 #include <mcheck.h>
