0a279888ccfbd4568ac0552880a44f298383d492
Thomas Adam <thomas@fvwm.org>
2023-08-29 21:42:44 +0100

    FvwmPager: add fpmonitor impls for widths/heights
    Module boundaries into libs/ in fvwm can sometimes be problematic.  As
    such, calling into X11 libraries with an assumed display variable which
    is initialised from fvwm, but not from modules, can sometimes mean this
    isn't guaranteed to work.
    
    Implement these X calls local to FvwmPager instead.
    
    Fixes #833

--- modules/FvwmPager/FvwmPager.h
+++ modules/FvwmPager/FvwmPager.h
@@ -45,6 +45,8 @@ struct fpmonitor		*fpmonitor_by_name(const char *);
 struct fpmonitor		*fpmonitor_by_output(int);
 struct fpmonitor		*fpmonitor_get_current(void);
 struct fpmonitor 		*fpmonitor_this(void);
+int				 fpmonitor_get_all_widths(void);
+int				 fpmonitor_get_all_heights(void);
 
 typedef struct ScreenInfo
 {
