c672c9f34be764273949c268acf2d12456cb9bc7
Thomas Adam <thomas@fvwm.org>
2023-11-28 23:19:44 +0000

    expand: use strtonum for parsing str to int
    When looking up numbers from a string, use strtonum() (from BSD) rather
    than atoi() so that we can distinguish between errors.

---
+++ libs/strtonum.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2004 Ted Unangst and Todd Miller
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef LIBS_STRTONUM_H
+#define LIBS_STRTONUM_H
+
+#include <stdlib.h>
+
+long long strtonum(const char *, long long, long long, const char **);
+
+#endif /* LIBS_STRTONUM_H */
