ca9ff717f9b829a88fe436ade0b13938cc5659bd
Thomas Adam <thomas@fvwm.org>
2024-03-24 12:39:13 +0000

    update_fvwm_monitor: don't broadcast monitor
    When updating windows as to which desk they're on, don't send out a
    change of monitor to modules, as this won't have changed.

--- fvwm/update.c
+++ fvwm/update.c
@@ -679,7 +679,6 @@ Bool update_fvwm_monitor(FvwmWindow *fw)
 	EWMH_SetCurrentDesktop(fw->m);
 	desk_add_fw(fw);
 	BroadcastConfig(M_CONFIGURE_WINDOW, fw);
-	BroadcastMonitorList(NULL);
 
 	return True;
 }
