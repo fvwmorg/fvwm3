daf25010cb5b6ca448e4e01b0deb67b9c8b10819
Thomas Adam <thomas@fvwm.org>
2024-10-01 13:16:36 +0000

    meson/muon: consolidate scripts & sanitise dist
    Move some of the build-system scripts into a top-level meson-scripts
    directly, and add a script (meson-scripts/dist-tarball.sh) to clean up
    the dist tarball before it's created.

---
+++ meson-scripts/dist-tarball.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Script to sanitise the tarball when calling "meson dist".  By default,
+# "meson dist" calls git-archive, which includes more files than the autotools
+# version of "make dist".
+
+# TODO: ".*" as a glob might be too much in the future; expand this out...
+FILES_TO_IGNORE=".*
+mkrelease.sh
+dev-docs"
+
+(
+	cd "$MESON_DIST_ROOT" && {
+    		IFS=$'\n'
+    		for f in $FILES_TO_IGNORE
+    		do
+        		echo "Removing $f from tarball..."
+        		rm -r "./$f"
+    		done
+	}
+)
